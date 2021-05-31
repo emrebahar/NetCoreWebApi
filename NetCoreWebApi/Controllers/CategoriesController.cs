50b7caadd0c3b8640a16378a68e97840427632f0
emre bahar <emrebahar24@hotmail.com>
2021-05-31 18:46:05 +0300

    Add project files.

---
+++ NetCoreWebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreWebApi.DAL.Context;
+using NetCoreWebApi.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using var context = new NetCoreWebApiContext();
+            return Ok(context.Categories.ToList());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using var context = new NetCoreWebApiContext();
+            var category = context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            using var context = new NetCoreWebApiContext();
+            //var updatedCategory = context.Categories.Find(category.Id);
+            var updatedCategory = context.Find<Category>(category.Id);
+            if (updatedCategory == null)
+            {
+                return NotFound();
+            }
+            updatedCategory.Name = category.Name;
+            context.Update(updatedCategory);
+            context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            using var context = new NetCoreWebApiContext();
+            var deletedCategory = context.Categories.Find(id);
+            if (deletedCategory == null)
+            {
+                return NotFound();
+            }
+            context.Remove(deletedCategory);
+            context.SaveChanges();
+            return NoContent();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            using var context = new NetCoreWebApiContext();
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return Created("", category);
+        }
+
+        [HttpGet("{id}/Blogs")]
+        public IActionResult GetWithBlogsById(int id)
+        {
+            using var context = new NetCoreWebApiContext();
+            var category = context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            var categoryWithBlogs = context.Categories.Where(I => I.Id == id).Include(I => I.Blogs).ToList();
+            return Ok(categoryWithBlogs);
+        }
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload([FromForm]IFormFile file)
+        {
+            if (file.ContentType == "image/jpeg")
+            {
+                var newFileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/documents/" + newFileName);
+                var stream = new FileStream(path, FileMode.Create);
+                await file.CopyToAsync(stream);
+                return Created("", file);
+            }
+            return BadRequest();
+            
+        }
+    }
+}
---
+++ NetCoreWebApi/DAL/Context/NetCoreWebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreWebApi.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApi.DAL.Context
+{
+    public class NetCoreWebApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-I0M32R9\\SQLEXPRESS; database=UdemyWebApi ; integrated security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
